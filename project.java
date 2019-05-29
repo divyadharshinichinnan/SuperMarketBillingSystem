3cb0bbf388278034fc0b74ac63c448b597bb35b9
divyadharshini <35074011+divyadharshinichinnan@users.noreply.github.com>
2019-05-29 12:37:11 +0530

    Create project.java

---
+++ project.java
@@ -0,0 +1,7 @@
+package project;
+public class Project {
+    public static void main(String[] args) {
+      login form=new login();
+      form.setVisible(true);// TODO code application logic here
+    }
+}
